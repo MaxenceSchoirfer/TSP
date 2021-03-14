eaa76d5f379d9576ae258bfea6c2701061054b7d
MaxenceSchoirfer <maxence.schoirfer@gmail.com>
2021-03-14 12:52:30 +0100

    final adam

--- src/tsp/projects/geneticopt/Adam.java
+++ src/tsp/projects/geneticopt/Adam.java
@@ -19,7 +19,7 @@ public class Adam extends CompetitorProject {
             this.currentProject = new GeneticLargeInstance(evaluation, 0.9, 0.4);
         else if (evaluation.getProblem().getLength() < 2000)
             this.currentProject = new GeneticLargeInstance(evaluation, 0.4, 0);
-        // else this.currentProject = new SimulatedAnnealing(evaluation);
+        //else this.currentProject = new SimulatedAnnealing(evaluation);
     }
 
     @Override
